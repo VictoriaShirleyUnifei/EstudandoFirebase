5024755c6f996bf9279416c27f2de1146859bc78
Victoria <victoriacatshirley@gmail.com>
2024-12-26 21:04:49 -0300

    Adicionando campos

--- App.js
+++ App.js
@@ -1,57 +1,94 @@
-import { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import { db } from './src/firebaseConnection';
-import { doc, getDoc, onSnapshot, setDoc, collection, addDoc } from 'firebase/firestore';
+import { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
+import { db } from "./src/firebaseConnection";
+import {
+  doc,
+  getDoc,
+  onSnapshot,
+  setDoc,
+  collection,
+  addDoc,
+} from "firebase/firestore";
 
 export default function App() {
-  const [nome, setNome] = useState("Carregando...")
+  const [nome, setNome] = useState("");
+  const [idade, setIdade] = useState("");
+  const [cargo, setCargo] = useState("");
+  const [showForm, setShowForm] = useState(true);
 
   useEffect(() => {
-    async function getDados(){
-      // const docref = doc(db, "users", "1");
-      // getDoc(docref)
-      // .then((snapshot) => {
-      //   setNome(snapshot.data()?.nome);
+    async function getDados() {
+      // onSnapshot(doc(db, "users", "1"), (doc) => {
+      //   setNome(doc.data()?.nome);
       // })
-      // .catch((err) => {
-      //   console.log("error: ", err);
-      // })
-
-      //observer in real time
-      onSnapshot(doc(db, "users", "1"), (doc) => {
-        setNome(doc.data()?.nome);
-      })
-    } 
+    }
 
     getDados();
-  }, [])
+  }, []);
 
-  async function handleRegister(){
-    // await setDoc(doc(db, "users", "3"), {
-    //   nome: "José",
-    //   idade: 30,
-    //   cargo: "Back-End"
-    // })
-    // .then(() => {
-    //   console.log("Cadastrado com sucesso!!");
-    // })
-    // .catch((err) => {
-    //   console.log(err);
-    // })
-
-    //id aleatório
+  async function handleRegister() {
     await addDoc(collection(db, "users"), {
-      nome: "Bianca",
-      idade: 20,
-      cargo: "Front-End"
+      nome: nome,
+      idade: idade,
+      cargo: cargo,
     })
+      .then(() => {
+        console.log("Usuário cadastrado com sucesso!");
+        setNome("");
+        setIdade("");
+        setCargo("");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  function handleToggle() {
+    setShowForm(!showForm);
   }
 
   return (
     <View style={styles.container}>
-      <Text style={{fontSize: 16}}>Nome: {nome}</Text>
-      <TouchableOpacity style={styles.button} onPress={handleRegister}>
-        <Text style={styles.buttonText}>Adicionar</Text>
+      {showForm && (
+        <View>
+          <Text style={styles.label}>Nome:</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Digite seu nome..."
+            value={nome}
+            onChangeText={(text) => setNome(text)}
+          />
+
+          <Text style={styles.label}>Idade:</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Digite sua idade..."
+            value={idade}
+            onChangeText={(text) => setIdade(text)}
+          />
+
+          <Text style={styles.label}>Cargo:</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Digite seu cargo..."
+            value={cargo}
+            onChangeText={(text) => setCargo(text)}
+          />
+
+          <TouchableOpacity style={styles.button} onPress={handleRegister}>
+            <Text style={styles.buttonText}>Adicionar</Text>
+          </TouchableOpacity>
+        </View>
+      )}
+
+      <TouchableOpacity style={styles.button} onPress={handleToggle}>
+        <Text style={styles.buttonText}>{ showForm ? "Esconder formulário" : "Mostrar formulário"}</Text>
       </TouchableOpacity>
     </View>
   );
@@ -61,13 +98,27 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 10,
-    paddingTop: 40,
+    paddingTop: 100,
   },
   button: {
     backgroundColor: "#000",
+    borderRadius: 8,
+    marginBottom: 4,
   },
   buttonText: {
     padding: 8,
-    color: "#fff"
-  }
+    color: "#fff",
+    textAlign: "center",
+  },
+  label: {
+    fontSize: 16,
+    marginBottom: 4,
+    fontWeight: "bold",
+  },
+  input: {
+    borderWidth: 1,
+    borderRadius: 8,
+    color: "gray",
+    marginBottom: 8,
+  },
 });
